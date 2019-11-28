a075f4de4855fd843389896f5740b0a63690d5ab
rlhuerta <45705107+rlhuerta@users.noreply.github.com>
2019-11-28 15:07:23 -0300

    Update puzzle.sh

--- scripts/puzzle.sh
+++ scripts/puzzle.sh
@@ -4,7 +4,7 @@
 BUILD_TAG=`git rev-parse --short HEAD`
 
 #Build the docker image
-docker build -t 127.0.0.1:30400/puzzle:latest -f applications/puzzle/Dockerfile applications/puzzle
+docker build -t 127.0.0.1:30400/puzzle:latest -f /home/rthuerta/kubernetes-ci-cd/applications/puzzle/Dockerfile applications/puzzle
 
 #Setup the proxy for the registry
 docker stop socat-registry; docker rm socat-registry; docker run -d -e "REG_IP=`minikube ip`" -e "REG_PORT=30400" --name socat-registry -p 30400:5000 socat-registry
@@ -19,5 +19,5 @@ docker push 127.0.0.1:30400/puzzle:latest
 docker stop socat-registry
 
 # Create the deployment and service for the puzzle server aka puzzle
-sed 's#127.0.0.1:30400/puzzle:$BUILD_TAG#127.0.0.1:30400/puzzle:latest#' applications/puzzle/k8s/deployment.yaml | kubectl apply -f -
+sed 's#127.0.0.1:30400/puzzle:latest#127.0.0.1:30400/puzzle:latest#' /home/rthuerta/kubernetes-ci-cd/applications/puzzle/k8s/deployment.yaml | kubectl apply -f -
 
